b9ea6bae286d64f47dbe0e8caedc620583eb8646
shreeyasguggari <guggarishreeyas@gmail.com>
2023-01-17 16:37:22 +0530

    API update

--- createrepo.sh
+++ createrepo.sh
@@ -5,6 +5,6 @@ read PAT
 curl\
 -X post \
 -u shreeyasguggari:$PAT \
-https://api.github.com/orgs/karnatakabank-apps/repos \
+https://api.github.com/orgs/Karnatakabank-app/repos \
 -d '{"name":"karnatakabank-GitHubAPI","Description":"Thi is your first repository created by using API"}'
 
